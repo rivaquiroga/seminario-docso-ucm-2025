707d44a3c6c27dc29cdf262c55db92d56584dc36
Riva Quiroga <riva.quiroga@uc.cl>
2025-06-26 17:02:28 -0400

    código ejercicio 5

--- codigo/5_api-biobio-chile.R
+++ codigo/5_api-biobio-chile.R
@@ -18,10 +18,8 @@ offset = "0"
   
 # Construir la URL para la API de Biobio 
 
-url <- paste0("https://www.biobiochile.cl/lista/api/buscador?offset=", 
-              offset, 
-              "&search=", 
-              search_encoded, 
+url <- paste0("https://www.biobiochile.cl/lista/api/buscador?offset=", offset, 
+              "&search=", search_encoded, 
               "&intervalo=&orden=ultimas")
 
 
@@ -30,17 +28,22 @@ url <- paste0("https://www.biobiochile.cl/lista/api/buscador?offset=",
 response <- GET(url)
 status_code(response) # siempre queremos que sea 200
 
-# convertir a datos brutos
+# extraemos el contenido de la respuesta y procesamos el JSON
 
 data_raw <- content(response, "text", encoding = "UTF-8")
 data_json <- fromJSON(data_raw, flatten = TRUE)
 
-total_resultados <- data_json$total
+total_resultados <- data_json$total # este valor es importante guardarlo en caso de que tengamos más de 20 respuestas (el máximo que entrega por cada petición la API)
 
+# seleccionamos algunas columnas y ajustamos el formato del contenido de cada noticia
 tabla_resultados <- as_tibble(data_json$notas) |> 
   select(ID, post_title, post_content, post_URL, raw_post_date) |> 
   rowwise() |> 
   mutate(post_content = read_html(post_content) |> html_text2())
 
-
 writexl::write_xlsx(tabla_resultados, "datos/extraccion-noticias-biobio.xlsx")
+
+# Para seguir explorando en el futuro: iterar en los casos en que el total de resultados sea mayor a 20.
+# Robustecer nuestro código: por ejemplo, hacer que status_code(response) == 200 sea una condición para que el resto del código se siga ejecutando
+
+
